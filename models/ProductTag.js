f3bf33af6dd3d30364b8d5cbae22a0687696733d
krod2016 <katrinajrodriguez1993@gmail.com>
2022-02-19 13:10:00 -0600

    updated models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
 
-const sequelize = require('../config/connection');
+class Tag extends Model {}
 
-class ProductTag extends Model {}
-
-ProductTag.init(
+Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'tag',
   }
 );
 
-module.exports = ProductTag;
+module.exports = Tag;
